bb596b63dfe9d8d37b3841688bb35a407bbe67bb
pxai <pello_altadill@cuatrovientos.org>
2017-07-06 17:50:00 +0200

    finally, the simplest works

--- src/main/java/io/pello/simplest/SimplestModule.java
+++ src/main/java/io/pello/simplest/SimplestModule.java
@@ -5,6 +5,6 @@ import com.google.inject.AbstractModule;
 public class SimplestModule extends AbstractModule {
 	  @Override 
 	  protected void configure() {
-	    //bind(Car.class).to(Car.class);
+	    //bind(io.pello.cleanbank.usecase.Login.class).to(io.pello.cleanbank.boundaries.LoginBoundary.class);
 	  }
 }
